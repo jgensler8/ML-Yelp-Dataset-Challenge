de82f0e0726814991466f9ce6a54f63910e585b5
Jeff Gensler <jgensler8@gmail.com>
2015-04-28 00:25:03 +0000

    updating

--- python/scripts/cluster.py
+++ python/scripts/cluster.py
@@ -8,58 +8,69 @@ from mpl_toolkits.mplot3d import axes3d
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from collections import Counter
 
 #select file
 labeledCSV = "/tmp/labeled-data/feature_averages_final_normalized.csv"
 
 data = pd.DataFrame.from_csv(labeledCSV)
-X = data[['service_svm','location_svm','price_svm']].values
+X = data[['name','service_svm','location_svm','price_svm']].values
 Y = data['rating'].values
 
 FITINT = True
 
-X_train, X_test, Y_train, Y_test = cross_validation.train_test_split( X, Y, test_size=0.2, random_state=0)
+X_train_all, X_test_all, Y_train, Y_test = cross_validation.train_test_split( X, Y, test_size=0, random_state=0)
+X_train_names = [x[:1][0] for x in X_train_all]
+X_train = [x[1:] for x in X_train_all]
+X_test_names = [x[:1][0] for x in X_test_all]
+X_test = [x[1:] for x in X_test_all]
 
 #
 # Clustering
 #
-labels = ['^', 'o', 'h', '*', '+' ,'x']
-colors = ["blue", "yellow", "green", "red", "black", "purple"]
+labels = ['^', 'o', 'h', '*', '+' ,'x', 's', 'p', 'D', '<', '>', 'v']
+colors = ["blue", "yellow", "green", "red", "black", "purple", "orange", "grey", "teal", "tan"]
 
-for size in range(3,6):
-    #fit the training data to clusters
+for size in range(2,5):
+    # Fit the training data to clusters
     c = cluster.SpectralClustering(n_clusters=size)
     c.fit(X_train)
     
-    #set up plot
-    fig = plt.figure()
+    # Set up plot
+    fig = plt.figure(1,figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
     ax.set_xlabel("service")
     ax.set_ylabel("location")
     ax.set_zlabel("price")
     
-    #gather our data to be potted
-    all_data = zip(c.labels_, X_train, Y_train)
-    all_data_normalized = [ {"array":array,"label":label,"prediction":prediction} for (label,array,prediction) in all_data]
+    # Gather our data to be potted
+    all_data = zip(c.labels_, X_train, Y_train, X_train_names)
+    all_data_normalized = [ {"array":array,"label":label,"prediction":prediction,"name":name} for (label,array,prediction,name) in all_data]
     
-    #calculate a regression fit for each cluster
     for cluster_label in range(0,size):
         c = filter(lambda point: point["label"] == cluster_label, all_data_normalized) 
-        linreg = linear_model.LinearRegression(fit_intercept=FITINT)
         X_s = [point["array"] for point in c]
         Y_s = [point["prediction"] for point in c]
+        
+        # Fit linear for each cluster
+        linreg = linear_model.LinearRegression(fit_intercept=FITINT)
         linreg.fit( X_s, Y_s)
         print("Size", size, "Cluster", cluster_label, "Coef", linreg.coef_, "Intercept", linreg.intercept_)
+        #print([round(y,1) for y in sorted(Y_s)])
+        
+        # Graph each cluster
         X = map(lambda x: x[0], X_s)
         Y = map(lambda x: x[1], X_s)
         Z = map(lambda x: x[2], X_s)
-        Label = np.average(Y_s)
+        mostCommonNames = Counter( [point["name"].lower() for point in c] ).most_common(2)
+        Label = str(round(np.average(Y_s),3)) + " " + str(mostCommonNames)
         ax.scatter( X, Y, Z, marker=labels[cluster_label], c=colors[cluster_label], label=Label)
      
-    #show the default legend which we have labeled above
-    ax.legend()
-    
-    plt.savefig("scatter_" + str(size) + ".png")
+    # Put legend to the right of graph
+    lgd = ax.legend(loc='center left', bbox_to_anchor=(1, .5),
+              fancybox=True, shadow=True, ncol=1)
+    # Extend the picture if needed
+    plt.savefig("scatter_" + str(size) + ".png",bbox_extra_artists=(lgd,), bbox_inches='tight')
     
 
 
